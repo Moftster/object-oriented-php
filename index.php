5c7dce682868b51e8fba0db5d98d0d3161264cce
David Moftakhar <david@sellmysnaps.com>
2019-10-21 17:16:15 +0100

    properties and methods section complete.

--- index.php
+++ index.php
@@ -1,3 +1,20 @@
 <?php 
 
+class Customer {
+    public $id;
+    public $name;
+    public $email;
+    public $balance;
+
+     public function getCustomer($id){
+        $this->id = $id;
+        return $this->id;
+     }
+}
+
+$customer = new Customer;
+
+echo $customer->getCustomer(11);
+
+
 ?>
